c0f4e943720490e415b16ed0c3960cf33fde4894
hegdeashwin <ashwin.hegde3@gmail.com>
2016-02-03 23:05:37 +0530

    Adds comment in context menu eg

--- codes/Ch3_Menu/Ch3_2_ContextMenu/script.js
+++ codes/Ch3_Menu/Ch3_2_ContextMenu/script.js
@@ -1,18 +1,39 @@
 /**
- *
+ * 1. Building application contextmenu.
+ * 2. Setting menu shortcuts.
+ * 3. Adding separator between submenu.
+ * 4. Using MenuItem and to use menu append method.
  */
-
 'use strict';
 
 const remote = require('electron').remote;
 const Menu = remote.Menu;
 const MenuItem = remote.MenuItem;
 
+/**
+ * Create menu instance.
+ */
 var menu = new Menu();
 
-menu.append(new MenuItem({ label: 'MenuItem1', click: function() { console.log('item 1 clicked'); } }));
-menu.append(new MenuItem({ type: 'separator' }));
-menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
+/**
+ * Pass MenuItem instance as a menu append parameter.
+ */
+menu.append(new MenuItem({
+  label: 'File',
+  click: function() {
+    console.log('item 1 clicked');
+  }
+}));
+
+menu.append(new MenuItem({
+  type: 'separator'
+}));
+
+menu.append(new MenuItem({
+  label: 'Help',
+  type: 'checkbox',
+  checked: true
+}));
 
 window.addEventListener('contextmenu', function (e) {
   e.preventDefault();
