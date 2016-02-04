84c67353cf58468e817369366a2cdd448c2b9e5e
hegdeashwin <ashwin.hegde3@gmail.com>
2016-02-05 01:06:16 +0530

    Updates comments

--- codes/Ch4_Dialog/Ch4_1_ShowOpenDialog/Eg1/script.js
+++ codes/Ch4_Dialog/Ch4_1_ShowOpenDialog/Eg1/script.js
@@ -1,5 +1,6 @@
 /**
- * 1. Using showOpenDialog to open the directory.
+ * 1. Using showOpenDialog to open the "Open dialog box".
+ * 2. Setting showOpenDialog to get the directory path.
  */
 'use strict';
 
@@ -8,10 +9,23 @@ const dialog = remote.dialog;
 
 document.getElementById('openDirectory').addEventListener('click', function() {
 
+  /**
+   * Notes:
+   * On Windows and Linux an open dialog can not be both a file selector and a directory selector,
+   * so if you set properties to ['openFile', 'openDirectory'] on these platforms,
+   * a directory selector will be shown.
+   */
   dialog.showOpenDialog({
-    properties: ['openDirectory']
-  }, function(data) {
-    document.getElementById('openDirectoryInput').value = data;
+    properties: ['openDirectory'], // Open Directory Parameter,
+    title: 'Select your project directory ...', // Set opendialog title.
+    defaultPath: '/home/hegdeashwin/Downloads' // Set the default path for opendialog box.
+  }, function(path) {
+
+    /**
+     * On success this method returns an array of file paths chosen by the user,
+     * otherwise it returns undefined.
+     */
+    document.getElementById('openDirectoryInput').value = path;
   });
 
 });
