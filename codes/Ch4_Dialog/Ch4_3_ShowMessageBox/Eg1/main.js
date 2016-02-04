ef1892e09f4418f8e1a43c769e8ac8c215166709
hegdeashwin <ashwin.hegde3@gmail.com>
2016-02-05 02:06:36 +0530

    Adds default message box eg

---
+++ codes/Ch4_Dialog/Ch4_3_ShowMessageBox/Eg1/main.js
@@ -0,0 +1,57 @@
+/**
+ * 1. Building application menu.
+ */
+'use strict';
+
+const electron = require('electron');
+
+/**
+ * Module to control application life cycle.
+ */
+const app = electron.app;
+
+/**
+ * Module to create native browser window.
+ */
+const BrowserWindow = electron.BrowserWindow;
+
+/**
+ * mainWindow keep a global reference of the window object, if you don't then window
+ * will be closed automatically when the JavaScript object is garbage collected.
+ */
+var mainWindow = null;
+
+/**
+ * 'ready' method will be called when Electron has finished initialization and
+ * is ready to create browser window.
+ */
+app.on('ready', function() {
+
+  /**
+   * Create the new browser window.
+   * There are lot more options to set please visit: http://electron.atom.io/docs/v0.36.5/api/browser-window/#class-browserwindow
+   */
+  mainWindow = new BrowserWindow();
+
+  /**
+   * Load the index.html of the app.
+   */
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  /**
+   * Open the DevTools for debugging.
+   */
+  // mainWindow.webContents.openDevTools();
+
+  /**
+   * Emitted when the window is closed.
+   */
+  mainWindow.on('closed', function() {
+
+    /**
+     * Derefeence the window object
+     */
+    mainWindow = null;
+  });
+
+});
---
+++ codes/Ch4_Dialog/Ch4_3_ShowMessageBox/Eg1/script.js
@@ -0,0 +1,38 @@
+/**
+ * 1. Using showMessageBox.
+ */
+'use strict';
+
+const remote = require('electron').remote;
+const dialog = remote.dialog;
+
+document.getElementById('noneMsgBox').addEventListener('click', function() {
+
+  /**
+   * Buttons is required field.
+   */
+  dialog.showMessageBox({
+    type: 'none',
+    buttons: ['Ok', 'Cancel'],
+    title: 'Hello World!', // message box title
+    message: 'Hello World! My name is Ashwin Hegde', // message box content title
+    detail: 'I work as Software Engineer and Open Source Contributor!' // message box content
+  }, function(index) {
+
+    /**
+     * Shows a message box, it will block the process until the message box is closed.
+     * It returns the index of the clicked button.
+     */
+    switch (index) {
+      case 0:
+        alert("You clicked 'ok'!");
+        break;
+
+      case 1:
+        alert("You clicked 'Cancel'!");
+        break;
+    }
+
+  });
+
+});
